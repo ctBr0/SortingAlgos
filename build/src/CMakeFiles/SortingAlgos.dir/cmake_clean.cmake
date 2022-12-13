e2e4d2dc4b05addc662738b4fb0a50819491d86b
ctBr0 <justin6008@icloud.com>
2022-12-12 23:34:06 -0800

    added cmake

---
+++ build/src/CMakeFiles/SortingAlgos.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../SortingAlgos"
+  "../SortingAlgos.pdb"
+  "CMakeFiles/SortingAlgos.dir/SortingAlgos.cpp.o"
+  "CMakeFiles/SortingAlgos.dir/SortingAlgos.cpp.o.d"
+  "CMakeFiles/SortingAlgos.dir/main.cpp.o"
+  "CMakeFiles/SortingAlgos.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SortingAlgos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
